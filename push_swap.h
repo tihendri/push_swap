b36f5adeda667490a664e42adc8f047222ee1df1
Timothy Hendricks <tihendri@c3r8s1.wethinkcode.co.za>
2019-07-19 10:28:13 +0200

    changed list structure

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tihendri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 09:44:48 by tihendri          #+#    #+#             */
-/*   Updated: 2019/07/18 16:12:11 by tihendri         ###   ########.fr       */
+/*   Updated: 2019/07/19 10:09:30 by tihendri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ typedef struct		s_lst
 	size_t	val;
 	size_t	coup;
 	struct	s_node	*head;
-	struct	s_node	*next;
+	struct	s_node	*tail;
 }					t_lst;
 
 # define INSTRUCTIONS 11
@@ -39,16 +39,16 @@ typedef struct		s_lst
 **from moves files, instructions
 */
 
-void				pa(t_lst **stack_a, t_lst **stack_b);
-void				pb(t_lst **stack_a, t_lst **stack_b);
-void				ra(t_lst **stack_a, t_lst **stack_b);
-void				rb(t_lst **stack_a, t_lst **stack_b);
-void				rr(t_lst **stack_a, t_lst **stack_b);
-void				rra(t_lst **stack_a, t_lst **stack_b);
-void				rrb(t_lst **stack_a, t_lst **stack_b);
-void				rrr(t_lst **stack_a, t_lst **stack_b);
-void				sa(t_lst **stack_a, t_lst **stack_b);
-void				sb(t_lst **stack_a, t_lst **stack_b);
-void				ss(t_lst **stack_a, t_lst **stack_b);
+void				pa(t_lst *stack_a, t_lst *stack_b);
+void				pb(t_lst *stack_a, t_lst *stack_b);
+void				ra(t_lst *stack_a, t_lst *stack_b);
+void				rb(t_lst *stack_a, t_lst *stack_b);
+void				rr(t_lst *stack_a, t_lst *stack_b);
+void				rra(t_lst *stack_a, t_lst *stack_b);
+void				rrb(t_lst *stack_a, t_lst *stack_b);
+void				rrr(t_lst *stack_a, t_lst *stack_b);
+void				sa(t_lst *stack_a, t_lst *stack_b);
+void				sb(t_lst *stack_a, t_lst *stack_b);
+void				ss(t_lst *stack_a, t_lst *stack_b);
 
 #endif
