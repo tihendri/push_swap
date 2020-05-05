0ef39c671eb6a8d6d08ea01439da919af8f63bfc
timothy <tihendri@student.wethinkcode.co.za>
2020-05-05 12:36:34 +0200

    added function and restructured median functions

--- push_swap_files/median.c
+++ push_swap_files/median.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   midpoint.c                                         :+:      :+:    :+:   */
+/*   median.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tihendri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,38 +12,41 @@
 
 #include "push_swap.h"
 
-int	get_count(t_stack *s)
+int		get_count(t_stack *stack)
 {
 	int		count;
 	t_lst	*temp;
 
-	temp = s->head;
+	temp = stack->head;
 	count = 0;
-	while (temp != s->p[s->start] && count < 12)
+	while (temp != stack->sub_lst[stack->start] && count < 10)
 	{
 		count++;
 		temp = temp->next;
 	}
-	if (!s->p[s->start] && count == 3)
+	if (!stack->sub_lst[stack->start] && count == 3)
 		return (-1);
 	return (count);
 }
 
 /*
-**Finds wherever i is and splits the total in half.
-**Uses a "ft_swap" kind of function at lines 58-60.
+**This function is used to get the median of the stack when there's fewer
+**arguments on stack A.
+**This heps with effieciency by lowering the amount of moves made when
+**the arguments are fewer.
+**This function is specifically created for the evaluation of this project.
 */
 
-int	real_median(t_stack *a)
+long	low_args_median_a(t_stack *a)
 {
 	t_lst	*temp;
-	int		arr[a->argc + 1];
+	int		arr[8];
 	int		i;
 	int		j[2];
 
 	temp = a->head;
 	i = 0;
-	while (temp != a->p[a->start])
+	while (temp != a->sub_lst[a->start])
 	{
 		arr[i++] = temp->value;
 		temp = temp->next;
@@ -55,9 +58,81 @@ int	real_median(t_stack *a)
 		while (++j[1] < i)
 			if (arr[j[0]] < arr[j[1]])
 			{
-				arr[a->argc] = arr[j[1]];
+				arr[7] = arr[j[1]];
 				arr[j[1]] = arr[j[0]];
-				arr[j[0]] = arr[a->argc];
+				arr[j[0]] = arr[7];
+			}
+	}
+	return ((!a->sub_lst[a->start]) ? arr[3] : arr[2]);
+}
+
+/*
+**This function is used to get the median of the stack when there's fewer
+**arguments on stack B.
+**This heps with effieciency by lowering the amount of moves made when
+**the arguments are fewer.
+**This function is specifically created for the evaluation of this project.
+*/
+
+long	low_args_median_b(t_stack *b)
+{
+	t_lst	*temp;
+	int		arr[5];
+	int		i;
+	int		j[2];
+
+	temp = b->head;
+	i = 0;
+	while (temp != b->sub_lst[b->start])
+	{
+		arr[i++] = temp->value;
+		temp = temp->next;
+	}
+	j[0] = -1;
+	while (j[0]++ < i)
+	{
+		j[1] = j[0];
+		while (++j[1] < i)
+			if (arr[j[0]] > arr[j[1]])
+			{
+				arr[4] = arr[j[1]];
+				arr[j[1]] = arr[j[0]];
+				arr[j[0]] = arr[4];
+			}
+	}
+	return ((!b->sub_lst[b->start]) ? arr[2] : arr[1]);
+}
+
+/*
+**Finds the actual median of the number set, in the stack.
+**This is the original median-finding function used in my
+**algorithm.
+*/
+
+int		real_median(t_stack *stack)
+{
+	t_lst	*temp;
+	int		arr[stack->argc + 1];
+	int		i;
+	int		j[2];
+
+	temp = stack->head;
+	i = 0;
+	while (temp != stack->sub_lst[stack->start])
+	{
+		arr[i++] = temp->value;
+		temp = temp->next;
+	}
+	j[0] = -1;
+	while (j[0]++ < i)
+	{
+		j[1] = j[0];
+		while (++j[1] < i)
+			if (arr[j[0]] < arr[j[1]])
+			{
+				arr[stack->argc] = arr[j[1]];
+				arr[j[1]] = arr[j[0]];
+				arr[j[0]] = arr[stack->argc];
 			}
 	}
 	return (arr[i / 2]);
