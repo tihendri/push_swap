f150b7db37a1eeae1f7184925cfe724aebe6a5ac
timothy <tihendri@student.wethinkcode.co.za>
2020-05-05 12:35:43 +0200

    removed infinite while loop

--- push_swap_files/main.c
+++ push_swap_files/main.c
@@ -33,7 +33,7 @@ void	free_all(t_stack *a, t_list *command)
 		command = command->next;
 		free(temp_a);
 	}
-	free(a->p);
+	free(a->sub_lst);
 }
 
 int		main(int ac, char **av)
@@ -43,23 +43,22 @@ int		main(int ac, char **av)
 	t_list	*command;
 
 	choose_stack(&a, &b, av, ac);
-	if (!(a.p = malloc(sizeof(t_lst*) * a.argc_temp)) ||
-	!(b.p = malloc(sizeof(t_lst*) * a.argc_temp)))
+	if (!(a.sub_lst = malloc(sizeof(t_lst*) * a.argc_temp)) ||
+	!(b.sub_lst = malloc(sizeof(t_lst*) * a.argc_temp)))
 		exit(1);
 	a.argc = a.argc_temp;
 	b.argc = a.argc_temp;
 	a.start = 0;
 	b.start = 0;
-	a.p[a.start] = NULL;
-	b.p[a.start] = NULL;
+	a.sub_lst[a.start] = NULL;
+	b.sub_lst[a.start] = NULL;
 	command = solver(&a, &b);
 	while (command != NULL)
 	{
 		ft_putstr((char *)command->content);
 		command = command->next;
 	}
-	free(b.p);
+	free(b.sub_lst);
 	free_all(&a, command);
-	// while (1);						//For memory leaks: run push_swap with this loop active, then "leaks push_swap"
 	return (0);
 }
