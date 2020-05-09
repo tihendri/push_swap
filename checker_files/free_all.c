6a9850c245082f9c4cc8e0b4e86ec9087cc92cfc
tihendri <tihendri@student.wethinkcode.co.za>
2020-05-09 09:41:45 +0200

    added old free_all function back to curb memleaks

---
+++ checker_files/free_all.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker_main.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tihendri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/20 11:53:04 by tihendri          #+#    #+#             */
+/*   Updated: 2019/11/21 14:39:06 by tihendri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checker.h"
+
+void	free_all(t_all *all)
+{
+	if (all->args && all->token == TOKEN_STR)
+		ft_array_free(&all->args);
+	if (all->int_stack_a)
+		free(all->int_stack_a);
+	if (all->int_stack_b)
+		free(all->int_stack_b);
+	free(all);
+}
